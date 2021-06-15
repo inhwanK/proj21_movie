759aba1fc1a1991724acf41f28116f24d7252af1
min gang <noonn8176@gmail.com>
2021-06-15 20:12:04 +0900

    0615-정민강 movieController - movieDetail 영화 번호 받아 올수 있게 ModelAndView로 변경

--- src/main/java/proj21_movie/controller/MovieController.java
+++ src/main/java/proj21_movie/controller/MovieController.java
@@ -1,18 +1,21 @@
 package proj21_movie.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MovieController {
 	
-	@RequestMapping("/movielist")
-	public String theater() {
+	@GetMapping("/movielist")
+	public String movieList() {
 		return "movie/box_office";
 	}
 	
-	@RequestMapping("/movielist/movieDetail/{no}")
-	public String theaterDetail() {
-		return "movie/movie_detail";
+	@GetMapping("/movie")
+	public ModelAndView movie(@RequestParam(value = "movNo") int movNo) {
+		ModelAndView mav = new ModelAndView("movie/movie_detail", "movNo", movNo);
+		return mav;
 	}
 }
