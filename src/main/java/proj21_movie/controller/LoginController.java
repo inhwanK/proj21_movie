c494e58ae02edaec4a393fa14de26718558ab161
ss820938ss <ss820938ss@gmail.com>
2021-06-28 18:05:05 +0900

    0628 박유진 수정

--- src/main/java/proj21_movie/controller/LoginController.java
+++ src/main/java/proj21_movie/controller/LoginController.java
@@ -1,21 +1,27 @@
 package proj21_movie.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Cookie;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import proj21_movie.dto.LoginCommand;
 import proj21_movie.dto.Member;
 import proj21_movie.service.AuthService;
 import proj21_movie.service.MemberService;
 
 @Controller
 public class LoginController {
-	
+
 	@Autowired
 	private AuthService authService;
 
@@ -40,23 +46,49 @@ public class LoginController {
 		return "login/loginfail";
 	}
 
-	// 로그인(성공)
-	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public String loginPOST(HttpServletRequest request, Member member, RedirectAttributes rttr) throws Exception {
+	// 로그인(성공), 쿠키(시도중)
+    @GetMapping
+    public String form(LoginCommand loginCommand, @CookieValue(value="REMEMBER", required = false) Cookie rCookie) {
+    	if (rCookie != null) {
+            loginCommand.setMemEmail(rCookie.getValue());
+            loginCommand.setRememEmail(true);
+        }
 
-        System.out.println("login 메서드 진입");
-        System.out.println("전달된 데이터 : " + member);
+        return "/login/login";
+    }
+    
+    
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public String loginPOST(LoginCommand loginCommand, HttpServletRequest request, Member member, 
+			RedirectAttributes rttr, HttpServletResponse response) throws Exception {
+
+		System.out.println("login 메서드 진입");
+		System.out.println("전달된 데이터 : " + member);
 
 		HttpSession session = request.getSession();
 		Member mem = memService.memberLogin(member);
+		
 		if (mem == null) { // 일치하지 않는 아이디, 비밀번호를 입력한 경우
 
 			int result = 0;
 			rttr.addFlashAttribute("result", result);
 			return "redirect:/login";
-		}
+		}   
+        
+        //로그인 성공
 		session.setAttribute("member", mem); // 일치하는 아이디, 비밀번호를 입력한 경우 (로그인 성공)
+		
+		//쿠키(제발좀)
+        Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getMemEmail());
+        rememberCookie.setPath("/");
+        if (loginCommand.isRememEmail()) {
+            rememberCookie.setMaxAge(60 * 60 * 24 * 7);
+        }else {
+            rememberCookie.setMaxAge(0);
+        }
+        response.addCookie(rememberCookie);
+		
+		
 		return "redirect:/loginsuccess";
 	}
-
 }
