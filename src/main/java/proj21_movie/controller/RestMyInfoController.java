b113be0e2a52b8d09d09ac5ff267fa828a1ab9f5
ss820938ss <ss820938ss@gmail.com>
2021-07-05 15:13:49 +0900

    0705 박유진 myinfo 작성중

---
+++ src/main/java/proj21_movie/controller/RestMyInfoController.java
@@ -0,0 +1,34 @@
+package proj21_movie.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import proj21_movie.dto.Member;
+import proj21_movie.service.MemberService;
+
+@RestController
+@RequestMapping("/api")
+public class RestMyInfoController {
+	
+	@Autowired
+	private MemberService memService;
+	
+	// 페이지 전체수정
+	@RequestMapping(value="/myinfo", method = RequestMethod.GET)
+	public String registerUpdateView() throws Exception{
+		return "mypage/myinfo";
+	}
+
+	@RequestMapping(value="/updateMember", method = RequestMethod.POST) // 나중에 value 인포 액션값
+	public String registerUpdate(Member member, HttpSession session) throws Exception{
+		
+		memService.modifyMember(member);
+		session.invalidate();
+		
+		return "redirect:/login";
+	}
+}
